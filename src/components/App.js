3184fd3d70094e853df671f811b1a4d360c07327
Rustam <rustam905@gmail.com>
2021-03-14 21:18:24 -0700

    Side bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import useLocalStorage from "../hooks/useLocalStorage";
 import Login from "./Login"
+import Dashboard from "./Dashboard";
 
 function App() {
-  const [id, setId] = useLocalStorage();
+  const [id, setId] = useLocalStorage("id");
 
   return (
     <div>
-      {id}
-      <Login onSubmitId ={setId} />
+      {id ? <Dashboard id = {id}/> :<Login onSubmitId ={setId} />} 
     </div>
   );
 }
---
+++ src/components/newContactModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function newContactModal() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default newContactModal
